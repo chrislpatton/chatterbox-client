533c7194b58b2ea2bea7cc2493ffbca9a3da666d
chrislpatton <chrislpatton@gmail.com>
2016-01-25 20:28:36 -0500

    passed another test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,15 +2,51 @@
 
 $(document).on('ready', function(){
   
-  window.app = {};
-  app.init =  function(){};
+  window.app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  friends: {},
+  
+  clearMessages: function(){
+    $('#chats').children().remove();
+  },
+  
+  addMessage: function(username, text, roomName){
+    $('#chats').append('<p>' + username + text + roomName + '</p>');  
+  },
+  
+  addRoom: function(roomName){
+    $('#roomSelect').append('<p>' + roomName + '</p>');  
+  },
+  
+  addFriend: function(friend){
+    // var isFriend = false;
+    $('.username').on('click', function(){
+      app.friends[friend] = true
+      // isFriend = !()
+        // isFriend = true;
+      // $.('this').('friend')
+    });  
+  },
+  //var friends = {};
+  
+  // addFriend: function(friendName){
+  //   app.friends[friendName] = true;
+  // },
+
+  
+  
+  };
+  
+  app.init =  function(){
+    this.fetch();
+  };
   
   //var JSONobj = JSON.stringify(obj);
   
   
   app.send = function(){
     $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     // dataType: 'json',
@@ -27,20 +63,18 @@ $(document).on('ready', function(){
   
   app.fetch = function(){
     $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: app.server,
     type: 'GET',
-    data: JSON.stringify(message),
-    // dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent. Data: ', data);
     },
     error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message. Error: ', data);
       }
     });
   };
+  
   var message = {
       username: 'Mel Brooks',
       text: 'It\'s good to be the king',
@@ -48,5 +82,5 @@ $(document).on('ready', function(){
       
   };
   
-  
+  app.init(); 
 });
