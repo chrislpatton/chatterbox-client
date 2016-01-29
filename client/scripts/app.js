04a597a98331ce1d2409d631345e6ffdf4ff137b
chrislpatton <chrislpatton@gmail.com>
2016-01-29 17:37:23 -0500

    pulling in message in the console

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,51 @@
 // YOUR CODE HERE:
+var app;
 
+$(document).on('ready', function(){
+  
+
+  app = {
+    
+    username: 'anonymous',
+    
+    roomname: 'lobby',
+    
+    server: 'https://api.parse.com/1/classes/chatterbox',
+      
+    init : function(){
+      
+      app.fetch();      
+    },
+    
+    fetch: function(){
+      
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: app.server,
+        type: 'GET',
+        data: {order: '-createdAt'},
+        contentType: 'application/json',
+        success: function (data) {
+          console.log('chatterbox: Message sent. Data: ', data);
+        },
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message. Error: ', data);
+        }
+      });
+    },
+    
+    send: function(){
+      
+    },
+    
+    addMessage: function(){
+      
+    },
+    
+    addFriend: function(){
+      
+    },
+    
+  };
+});
